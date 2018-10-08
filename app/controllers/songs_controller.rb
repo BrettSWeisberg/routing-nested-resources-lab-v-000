3cf97d83b8a05fafa9e25d5dd17845f3680ba1d9
Brett Weisberg <weisberg.brett@gmail.com>
2018-10-08 21:51:16 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,12 +16,16 @@ end
   def show
     #SongsController GET show with  artist redirects to artists songs when artist song not found
     binding.pry
-    if Song.find_by(id: params[:id]) == nil
-       flash[:alert] = "Song not found."
-      redirect_to artists_path
-    else
-    @song = Song.find(params[:id])
-  end
+   if params[:artist_id] != nil
+     @artist = Artist.find_by(id: params[:artist_id])
+     @song = Song.find_by(id: params[:id])
+
+      if Song.find_by(id: params[:id]) == nil
+         flash[:alert] = "Song not found."
+         redirect_to artists_path
+      else
+          @song = Song.find(params[:id])
+      end
 end
 
   def new
